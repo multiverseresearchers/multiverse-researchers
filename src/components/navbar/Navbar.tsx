ed3440c24a279f3cf4b33cb9bd26599e53a784d2
Revati Tambe <24revati@gmail.com>
2024-04-06 21:08:59 -0700

    routing w/ router-dom

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,20 @@
+import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
-  return (
-    <nav>Navbar</nav>
-  )
+    return (
+        <nav>
+            <Link to={"/"}><img src="/logo.png" alt="" /> Multiverse Researchers</Link>
+            <ul>
+                <li><Link to={"/home"}>HOME</Link></li>
+                <li><Link to={"/about"}>ABOUT</Link></li>
+                <li><Link to={"/research"}>RESEARCH</Link></li>
+                <li><Link to={"/publications"}>PUBLICATIONS</Link></li>
+                <li><Link to={"/contact"}>CONTACT</Link></li>
+            </ul>
+            {/* add search bar feature */}
+        </nav>
+    )
 }
 
 export default Navbar
