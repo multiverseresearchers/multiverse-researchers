e72ab1dab241e0360f92097ebc8d491920da8f52
Revati Tambe <24revati@gmail.com>
2024-10-24 15:35:56 -0700

    updated home page w/new group info

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -22,64 +22,63 @@ const Home = () => {
                 <a href="https://daztyxpv.formester.com/f/6e4ea898-ec48-41c9-9570-fbe35facdc17" target="_blank"><div className="text-lg bg-pink-300 hover:bg-pink-900 hover:text-white transition-colors duration-300 rounded-full py-2 px-6 cursor-pointer my-4">JOIN US</div></a>
             </div>
 			<div className="flex flex-col w-full items-center">
-				<Tabs defaultValue="bio" className="w-2/3 p-1">
+				<Tabs defaultValue="physics" className="w-2/3 p-1">
 					<TabsList className="flex flex-row justify-evenly h-fit bg-white shadow-lg border border-gray-100 rounded-lg mb-12">
-						<TabsTrigger className="text-xs hover:bg-gray-100 focus:bg-gray-100 focus:text-blue-500 focus:font-semibold p-2" value="physics">Physics Group</TabsTrigger>
-						<TabsTrigger className="text-xs hover:bg-gray-100 focus:bg-gray-100 focus:text-blue-500 focus:font-semibold p-2" value="bio">Biology Group</TabsTrigger>
+						{/* <TabsTrigger className="text-xs hover:bg-gray-100 focus:bg-gray-100 focus:text-blue-500 focus:font-semibold p-2" value="bio">Biology Group</TabsTrigger> */}
 						<TabsTrigger className="text-xs hover:bg-gray-100 focus:bg-gray-100 focus:text-blue-500 focus:font-semibold p-2" value="envisci">Environment Science Group</TabsTrigger>
-						<TabsTrigger className="text-xs hover:bg-gray-100 focus:bg-gray-100 focus:text-blue-500 focus:font-semibold p-2" value="compsci">Computer Science Group</TabsTrigger>
-                        <TabsTrigger className="text-xs hover:bg-gray-100 focus:bg-gray-100 focus:text-blue-500 focus:font-semibold p-2" value="econ">Economics Group</TabsTrigger>
                         <TabsTrigger className="text-xs hover:bg-gray-100 focus:bg-gray-100 focus:text-blue-500 focus:font-semibold p-2" value="astronomy">Astronomy Group</TabsTrigger>
+                        <TabsTrigger className="text-xs hover:bg-gray-100 focus:bg-gray-100 focus:text-blue-500 focus:font-semibold p-2" value="physics">Physics Group</TabsTrigger>
+                        <TabsTrigger className="text-xs hover:bg-gray-100 focus:bg-gray-100 focus:text-blue-500 focus:font-semibold p-2" value="econ">Economics Group</TabsTrigger>
+                        <TabsTrigger className="text-xs hover:bg-gray-100 focus:bg-gray-100 focus:text-blue-500 focus:font-semibold p-2" value="compsci">Computer Science Group</TabsTrigger>
                         <TabsTrigger className="text-xs hover:bg-gray-100 focus:bg-gray-100 focus:text-blue-500 focus:font-semibold p-2" value="polisci">Political Science Group</TabsTrigger>
                         <TabsTrigger className="text-xs hover:bg-gray-100 focus:bg-gray-100 focus:text-blue-500 focus:font-semibold p-2" value="math">Mathematics Group</TabsTrigger>
 					</TabsList>
 
-                    {/* WAITING FOR MATH, PHYSICS, AND CS RESPONSES */}
 					<div className="p-8 bg-white border border-gray-100 rounded-lg shadow-xl">
-						<TabsContent value="physics">
-							<CircleSnippet
-								title="Exploring State-of-the-Art Monte Carlo Methods in Lattice QCD Simulations"
-								description={`This project analyzes the latest Monte Carlo methods employed in lattice Quantum Chromodynamics (QCD) simulations. It highlights how these advanced techniques enhance our comprehension of Quantum Chromodynamics phenomena, providing deeper insights into the fundamental forces governing particle interactions.`}
-								image1=""
-								image2=""
-							/>
-						</TabsContent>
-						<TabsContent value="bio">
-                            <CircleSnippet
-    								title="Environmental Toxins and Parkinson’s Disease Progression"
-    								description={`In the Biology Group, we will study and delve into research in topics ranging from molecular biology to ecology as well as genetics and biotechnology. Those who participate in this group will gather experience in biology and learn more about the biological world and why we are alive. We will utilize important skills such as data analysis and attention to detail, both highly important skills when analyzing biological functions. The goal of this group is for members to gain a more well-rounded understanding of biology research as well as a newfound passion for the life sciences! Here, we strive to learn more about readily available information and potentially learn new information that could help fuel biology research in the future.`}
-    								image1=""
-    								image2=""
-							/>
-                        </TabsContent>
-						<TabsContent value="envisci">
+                        <TabsContent value="envisci">
                             <CircleSnippet
     								title="Impact of Climate Change on Marginalized Communities"
     								description={`Our research group is dedicated to studying how environmental changes—such as rising sea levels, pollution, coastal erosion, and climate shifts—affect the quality of life in marginalized communities. We utilize collaborative research efforts, data analysis, and innovative approaches to address urgent global environmental issues. The group investigates a variety of environmental, social, economic, and cultural factors to enhance understanding and promote sustainable solutions that improve living conditions for those most vulnerable to environmental stress.`}
-    								image1=""
-    								image2=""
-							/>
-                        </TabsContent>
-						<TabsContent value="compsci">
-                            <CircleSnippet
-    								title="Ethical Implications of Quantum Mechanics"
-    								description={`Quantum ethics explores the intersection of ethical principles and the emerging field of quantum mechanics, specifically how it could influence moral decision-making. This interdisciplinary area challenges conventional ethical theories by means of suggesting new perspectives on issues like privacy, loose will, and the impact of our actions in a quantum world. We plan to delve deep into this significant and fascinating subject and answer questions that arise in our study.`}
-    								image1=""
-    								image2=""
-							/>
-                        </TabsContent>
-                        <TabsContent value="econ">
-                            <CircleSnippet
-    								title="The Impact of COVID-19 on the Housing Market"
-    								description={`The objective of the Economics Research group is to delve into why and how bank failures occur, namely the infamous Silicon Valley Bank, WashuM, and Lehman Brothers. We utilize collaborative research efforts, data analysis, and innovative approaches to address problems with banks and how we can overcome such disasters. We plan on analyzing each of the 3 banks and give a comparitive analysis, overview, consequences, and proactive measures to avoid future failures. We plan on using and analyzing Income Statements, Balance sheets, stock prices before and after failure, management issue analyzation, etc.`}
-    								image1=""
-    								image2=""
+    								image1="/imgs/circles/envisci/envisci1.png"
+    								image2="/imgs/circles/envisci/envisci2.png"
 							/>
                         </TabsContent>
                         <TabsContent value="astronomy">
                             <CircleSnippet
     								title="The Impact of COVID-19 on the Housing Market"
     								description={`The objective of the Astronomy research group is to discover unidetified celestial bodies by observing trends in data to find a potential match to behavior the implies that a celestial body might be orbiting a star. We will do this by using what is known as the transit method. The transit method is used to find an exoplanet orbiting a distant star, this method includes looking for a transit curve which is when the planet passes in between in us and the star causing a drop in luminoisty. This group will also utilise multiple datasets an algrorithm and collaborative research in order to attain this objective. By working together and analyizing as much data as we can this group's goal is to find fact checked evidence that our target stars have celestial bodies orbiting them.`}
+    								image1="/imgs/circles/astronomy/astronomy1.png"
+    								image2="/imgs/circles/astronomy/astronomy2.png"
+							/>
+                        </TabsContent>
+                        <TabsContent value="physics">
+							<CircleSnippet
+								title="Exploring State-of-the-Art Monte Carlo Methods in Lattice QCD Simulations"
+								description={`This project analyzes the latest Monte Carlo methods employed in lattice Quantum Chromodynamics (QCD) simulations. It highlights how these advanced techniques enhance our comprehension of Quantum Chromodynamics phenomena, providing deeper insights into the fundamental forces governing particle interactions.`}
+								image1="/imgs/circles/physics/physics1.png"
+								image2="/imgs/circles/physics/physics2.png"
+							/>
+						</TabsContent>
+                        <TabsContent value="econ">
+                            <CircleSnippet
+    								title="How and Why Banking Failures Occur"
+    								description={`The objective of the Economics Research group is to delve into why and how bank failures occur, namely the infamous Silicon Valley Bank, WashuM, and Lehman Brothers. We utilize collaborative research efforts, data analysis, and innovative approaches to address problems with banks and how we can overcome such disasters. We plan on analyzing each of the 3 banks and give a comparitive analysis, overview, consequences, and proactive measures to avoid future failures. We plan on using and analyzing Income Statements, Balance sheets, stock prices before and after failure, management issue analyzation, etc.`}
+    								image1="/imgs/circles/econ/econ1.png"
+    								image2="/imgs/circles/econ/econ2.png"
+							/>
+                        </TabsContent>
+						{/* <TabsContent value="bio">
+                            <CircleSnippet
+    								title="Environmental Toxins and Parkinson’s Disease Progression"
+    								description={`In the Biology Group, we will study and delve into research in topics ranging from molecular biology to ecology as well as genetics and biotechnology. Those who participate in this group will gather experience in biology and learn more about the biological world and why we are alive. We will utilize important skills such as data analysis and attention to detail, both highly important skills when analyzing biological functions. The goal of this group is for members to gain a more well-rounded understanding of biology research as well as a newfound passion for the life sciences! Here, we strive to learn more about readily available information and potentially learn new information that could help fuel biology research in the future.`}
+    								image1="/imgs/circles/bio/bio1.png"
+    								image2="/imgs/circles/bio/bio2.png"
+							/>
+                        </TabsContent> */}
+						<TabsContent value="compsci">
+                            <CircleSnippet
+    								title="Ethical Implications of Quantum Mechanics"
+    								description={`Quantum ethics explores the intersection of ethical principles and the emerging field of quantum mechanics, specifically how it could influence moral decision-making. This interdisciplinary area challenges conventional ethical theories by means of suggesting new perspectives on issues like privacy, loose will, and the impact of our actions in a quantum world. We plan to delve deep into this significant and fascinating subject and answer questions that arise in our study.`}
     								image1=""
     								image2=""
 							/>
@@ -94,8 +93,8 @@ const Home = () => {
                         </TabsContent>
                         <TabsContent value="math">
                             <CircleSnippet
-    								title="The Impact of COVID-19 on the Housing Market"
-    								description={`This project examines the profound effects of the COVID-19 pandemic on the housing market. Beginning anew, our study will investigate shifts in housing demand, pricing trends, and market dynamics influenced by the pandemic. By analyzing data and trends, we aim to provide insights into the evolving landscape of housing in the wake of COVID-19, informing future research directions and policy considerations.`}
+    								title="Coming soon..."
+    								description={`Coming soon...`}
     								image1=""
     								image2=""
 							/>
