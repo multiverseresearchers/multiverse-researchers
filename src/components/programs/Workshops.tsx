8ce81934c6b767fbe2da5d43a808efcfd343e8e8
beranki <bhargav.eranki@gmail.com>
2024-10-28 18:52:59 -0700

    added slight change to text centering

--- src/components/programs/Workshops.tsx
+++ src/components/programs/Workshops.tsx
@@ -35,8 +35,8 @@ const Workshops = () => {
 const WorkshopBlock = (props: any) => {
   return (
     <a target="_blank" href={props.link}>
-      <div className="p-6 flex flex-col items-center w-1/2 md:w-3/4 h-full m-auto hover:font-bold hover:border-b-4 hover:border-pink-300 hover:bg-gray-300 bg-gray-200 rounded-lg transition-all duration-300">
-        <h1 className="text-xl text-center">
+      <div className="text-center p-6 flex flex-col items-center w-1/2 md:w-3/4 h-full m-auto hover:font-bold hover:border-b-4 hover:border-pink-300 hover:bg-gray-300 bg-gray-200 rounded-lg transition-all duration-300">
+        <h1 className="text-xl">
           {props.name}
         </h1>
         <p className="text-lg m-2">{props.date}</p>
