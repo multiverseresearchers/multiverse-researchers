9a7353cd35eb52c78faa8d9e7553a199d56c9ba9
Revati Tambe <24revati@gmail.com>
2024-10-28 20:42:28 -0700

    home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -144,9 +144,10 @@ const Home = () => {
 				<h2 className='text-2xl sm:text-3xl lg:text-4xl font-semibold text-center py-8 text-gray-800'>Our Services</h2>
 
 				<div className="m-auto w-4/5 flex flex-col md:flex-row justify-evenly gap-8 lg:w-2/3">
-					<div className="md:w-1/3 bg-white shadow-lg rounded-lg p-6 flex flex-col justify-between hover:shadow-2xl transition-shadow duration-300">
+					<div className="md:w-1/3 bg-white shadow-lg rounded-lg px-6 py-8 flex flex-col justify-between hover:shadow-2xl transition-shadow duration-300">
 						<p className="text-xl lg:text-2xl font-semibold text-center text-gray-800">ReWire Program</p>
-						<p className="text-center py-4 text-gray-600">ReWire offers free, personalized research mentorship and project feedback for novice researchers, helping refine current projects and strengthen fundamental research skills for future success.</p>
+						<img className="w-1/2 h-40 object-contain m-auto" src="/imgs/services/rewire.webp" alt="" />
+						<p className="text-center py-4 text-gray-600 flex-grow">ReWire offers free, personalized research mentorship and project feedback for novice researchers, helping refine current projects and strengthen fundamental research skills for future success.</p>
 						<Link to={"/rewire"}>
 							<div className="w-fit m-auto text-lg bg-pink-400 hover:bg-pink-700 text-white transition-colors duration-300 rounded-full py-3 px-8 cursor-pointer">
 								Learn More
@@ -154,9 +155,10 @@ const Home = () => {
 						</Link>
 					</div>
 
-					<div className="md:w-1/3 bg-white shadow-lg rounded-lg p-6 flex flex-col justify-between hover:shadow-2xl transition-shadow duration-300">
+					<div className="md:w-1/3 bg-white shadow-lg rounded-lg px-6 py-8 flex flex-col justify-between hover:shadow-2xl transition-shadow duration-300">
 						<p className="text-xl lg:text-2xl font-semibold text-center text-gray-800">Workshops</p>
-						<p className="text-center py-4 text-gray-600">Through various initiatives, Multiverse Researchers have run multiple workshops to platform and educate members on research and educational aspects. Below is a catalog of our recent efforts.</p>
+						<img className="w-1/2 h-40 object-contain m-auto" src="/imgs/services/workshops.webp" alt="" />
+						<p className="text-center py-4 text-gray-600 flex-grow">Through various initiatives, Multiverse Researchers have run multiple workshops to platform and educate members on research and educational aspects. Below is a catalog of our recent efforts.</p>
 						<Link to={"/rewire"}>
 							<div className="w-fit m-auto text-lg bg-pink-400 hover:bg-pink-700 text-white transition-colors duration-300 rounded-full py-3 px-8 cursor-pointer">
 								Learn More
@@ -164,9 +166,10 @@ const Home = () => {
 						</Link>
 					</div>
 
-					<div className="md:w-1/3 bg-white shadow-lg rounded-lg p-6 flex flex-col justify-between hover:shadow-2xl transition-shadow duration-300">
+					<div className="md:w-1/3 bg-white shadow-lg rounded-lg px-6 py-8 flex flex-col justify-between hover:shadow-2xl transition-shadow duration-300">
 						<p className="text-xl lg:text-2xl font-semibold text-center text-gray-800">Summer Program Application Help & Cold Email Assistance</p>
-						<p className="text-center py-4 text-gray-600">Coming soon...</p>
+						<img className="w-1/2 h-40 object-contain m-auto" src="/imgs/services/cold-email.webp" alt="" />
+						<p className="text-center py-4 text-gray-600 flex-grow">Coming soon...</p>
 						<Link to={"/cold-email-assistance"}>
 							<div className="w-fit m-auto text-lg bg-pink-400 hover:bg-pink-700 text-white transition-colors duration-300 rounded-full py-3 px-8 cursor-pointer">
 								Learn More
