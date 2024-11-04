be1b5e40eb479fd357825ed831347a2e23bd0d5a
Revati Tambe <24revati@gmail.com>
2024-11-04 11:33:33 -0800

    minor gui changes

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -19,7 +19,7 @@ const Home = () => {
 					<p className='text-center w-2/3'>We are a student led organization that empowers high schoolers to engage in innovative research.</p>
 				</div>
 				<svg className='animate-bounce p-0 m-0' xmlns="http://www.w3.org/2000/svg" width="40px" height="40px" viewBox="0 0 24 24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C12.5523 3 13 3.44772 13 4V17.5858L18.2929 12.2929C18.6834 11.9024 19.3166 11.9024 19.7071 12.2929C20.0976 12.6834 20.0976 13.3166 19.7071 13.7071L12.7071 20.7071C12.3166 21.0976 11.6834 21.0976 11.2929 20.7071L4.29289 13.7071C3.90237 13.3166 3.90237 12.6834 4.29289 12.2929C4.68342 11.9024 5.31658 11.9024 5.70711 12.2929L11 17.5858V4C11 3.44772 11.4477 3 12 3Z" fill="#000000" /></svg>
-				<a href="https://daztyxpv.formester.com/f/6e4ea898-ec48-41c9-9570-fbe35facdc17" target="_blank"><div className="text-lg bg-pink-300 hover:bg-pink-900 hover:text-white transition-colors duration-300 rounded-full py-2 px-6 cursor-pointer my-4">JOIN US</div></a>
+				<a href="https://daztyxpv.formester.com/f/6e4ea898-ec48-41c9-9570-fbe35facdc17" target="_blank"><div className="text-xl bg-pink-300 hover:bg-pink-900 hover:text-white transition-colors duration-300 rounded-full py-4 px-8 cursor-pointer my-4">JOIN US</div></a>
 			</div>
 			<div className="hidden md:flex flex-col w-full items-center">
 				<Tabs defaultValue="physics" className="w-2/3 p-1">
--- src/components/publications/Publications.tsx
+++ src/components/publications/Publications.tsx
@@ -4,7 +4,7 @@ const Publications = () => {
   return (
     <div>
       <div className='flex flex-col items-center justify-center h-[50vh]'>
-        <h1 className='text-center sm:text-2xl md:text-3xl lg:text-4xl m-5'><span className="font-semibold">Published Papers</span></h1>
+        <h1 className='text-center sm:text-2xl md:text-3xl lg:text-4xl m-5'><span className="font-semibold">Paper Spotlight</span></h1>
         <div>
           <h1 className='text-center sm:text-md md:text-md lg:text-lg m-5'>This page is under maintenance. We&apos;ll be back soon!</h1>
         </div>
